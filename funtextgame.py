dab18bbfeb4e1612dd11031e0e84f72df2c25f1a
bitbloxhub <jonahkamp@gmail.com>
2020-11-25 18:34:30 -0800

    fun text game

---
+++ funtextgame.py
@@ -0,0 +1,15 @@
+import shlex
+
+def handle(cmd,args):
+	print(cmd)
+	print(args)
+
+def main():
+	while True:
+		cmdargs=shlex.split(input(">"))
+		cmd=cmdargs[0]
+		args=cmdargs[1:]
+		handle(cmd,args)
+
+if __name__ == "__main__":
+	main()
