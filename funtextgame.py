3456d426a597d4d4bc679ef2f51326991671690a
bitbloxhub <jonahkamp@gmail.com>
2020-11-25 18:41:06 -0800

    added custom prompt

--- funtextgame.py
+++ funtextgame.py
@@ -1,6 +1,7 @@
 # understanding this see https://stackoverflow.com/a/509295
 
 import shlex
+import os
 
 def handle(cmd,args):
 	print(cmd)
@@ -8,7 +9,7 @@ def handle(cmd,args):
 
 def main():
 	while True:
-		cmdargs=shlex.split(input(">"))
+		cmdargs=shlex.split(input(os.environ.get("PYFUNTEXTPROMPT",">")))
 		cmd=cmdargs[0]
 		args=cmdargs[1:]
 		handle(cmd,args)
