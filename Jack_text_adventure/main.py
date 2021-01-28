be2978b90447cb033364752d104d1ccba56a7b08
Jack Boreczky <jackboreczky@gmail.com>
2021-01-27 19:02:00 -0800

    Update text adventure
    Got to conditionals, added some checks to name

---
+++ Jack_text_adventure/main.py
@@ -0,0 +1,16 @@
+# Types of values:
+string = "this is a string"
+
+name = input("Hello! please tell me who you are: ")
+favorite_animal = input("What's your favorite animal? ")
+
+print(name)
+
+if name == "Jack":
+    print("You're our favorite!")
+elif name == "Luke":
+    print("You're not allowed into my program")
+else:
+    print("You're not Jack")
+
+print("Goodbye!")
