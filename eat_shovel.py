623f89ec007b444058d14cb815cb392929a48d31
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2021-02-24 18:46:09 -0800

    made Eat Shovel FUN!

--- eat_shovel.py
+++ eat_shovel.py
@@ -1,10 +1,6 @@
+import readline as _readline
 import abc
 import shlex
-
-class Interactible(abc.ABC):
-    @abc.abstractmethod
-    def interact(self, args):
-        pass
 
 class Playable(abc.ABC):
     @abc.abstractmethod
@@ -22,6 +18,20 @@ class BaseEntity(abc.ABC):
         if len(maching) == 1:
             maching[0].use(self)
 
+class Player(BaseEntity, Playable):
+    def __init__(self, start_room, cmd_map):
+        BaseEntity.__init__(self, "NuKitty cat", start_room)
+        self.cmd_map = cmd_map
+
+    def play(self):
+        while True:
+            action = shlex.split(input(f"{self.name} is in {self.room.roomname} > "))
+            action_func = self.cmd_map.get(action[0], None)
+            if action_func:
+                action_func(self, action)
+            else:
+                print("sorry, but i do not understand.")
+
 class BaseItem(abc.ABC):
     def __init__(self, name):
         self.name = name
@@ -31,22 +41,37 @@ class BaseItem(abc.ABC):
         pass
 
 class Room():
-    def __init__(self, directionroom = {}):
+    def __init__(self, roomname = "A Normal Room", description = "Nothing special here, just a room", directionroom = {}, items = []):
         self.roommap = directionroom
+        self.roomname = roomname
+        self.roomdesc = description
 
     def getWalkRoom(self, direction):
         return self.roommap.get(direction, None)
 
-class Game:
+class Game():
     def __init__(self, player):
         self.player = player
 
     def play(self):
         self.player.play()
 
+# underscore b/c we dont use cmd
+def act_nuke(player, _cmd):
+    print("BOOM!")
+
 def main():
     # everything you run on startup is in here (unless its global init)
-    pass
+    start_room = Room()
+    cmd_map = {
+        "nuke": act_nuke
+    }
+    player = Player(start_room, cmd_map)
+    game = Game(player)
+    try:
+        game.play()
+    except EOFError as eoferr:
+        print("\nBye!")
 
 if __name__ == '__main__':
     main()
