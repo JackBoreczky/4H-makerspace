f81a2ca74cb90283da8328c264b4e4df8af7e082
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2021-05-26 18:27:14 -0700

    commit with lazygit

--- obfusco.py
+++ obfusco.py
@@ -1,1 +1,5 @@
-print((lambda l:[n for n in l if n%100==0])(range(500)))
+import time
+
+#print((lambda l:[n for n in l if n%100==0])(range(500)))
+
+print((lambda a,b,c:a+b+c)(time.time(),time.time(),time.time()))
